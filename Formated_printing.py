f04384ad5e726630df563b0246eeb106b42106fb
Ketan Thakur <76142366+ketanthakurr@users.noreply.github.com>
2022-01-09 12:53:33 +0530

    Add files via upload

---
+++ Formated_printing.py
@@ -0,0 +1,19 @@
+# "END PARAMETER"
+'''
+for x in range(11):
+    #print(x) #This print command will execute the command in every diffrent line
+    print(x, end =' ') #This command will execute the output in a single line
+'''
+# "SEP PARAMETER"
+
+'''
+DD = 18
+MM = 10
+YYYY = 2003
+#print ("Your Birthday Date is", DD, MM, YYYY) 
+# In this the DD MM YYYY is not seprated by any symbol
+# To fix this we use seprate operator to use any symbol to seperate the output statement
+print ("Your Birthday Date is", end= ' ') # we used the end parameter so that the output will be insame line..!!
+print (DD,MM,YYYY, sep='/')# sep parameter to seperate the output..!!
+'''
+
